da1e5820bada1b948351f915cd28df6d314f5d6a
Tom <87855471+Tom2612@users.noreply.github.com>
2022-05-16 14:48:31 +0800

    Add files via upload
    Started getting background images

---
+++ backgroundImages.js
@@ -0,0 +1,20 @@
+console.log('hello world')
+
+
+const backgrounds = {
+    Thunderstorm: 'https://images.unsplash.com/photo-1500674425229-f692875b0ab7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+    Rain: 'https://images.unsplash.com/photo-1428592953211-077101b2021b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1674&q=80',
+    Clouds: 'https://images.unsplash.com/photo-1473662107208-bd0a13f73559?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2122&q=80',
+    Drizzle: 'https://images.unsplash.com/photo-1508873760731-9c3d0bb6b961?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+    Snow: 'https://images.unsplash.com/photo-1542601098-8fc114e148e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80',
+    Clear: 'https://images.unsplash.com/photo-1469122312224-c5846569feb1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2189&q=80',
+    Dust: 'c',
+    Mist: 'c',
+    Squall: 'c',
+    Haze: 'c',
+    Smoke: 'c',
+    Fog: 'c',
+    Sand: 'c',
+    Ash: 'c',
+    Tornado: 'c',
+}
